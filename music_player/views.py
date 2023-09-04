ba419dd908b0584d76dc7d9d74fa006a112b70be
DuhLessMax <130593272+DuhLessMax@users.noreply.github.com>
2023-09-04 20:50:05 +0300

    test

--- music_player/views.py
+++ music_player/views.py
@@ -4,4 +4,5 @@ from .models import *
 # Create your views here.
 def home(request):
     music = Song.objects.all()
+    print('test') # TODO delete it then
     return render(request, 'home.html', {'songs':music})
