d73b3b5efa8c545e4385ee004c860f4605d545a5
mac <mac@MacBook-Air-mac.local>
2023-09-06 19:34:17 +0300

    second_step

--- music_player/views.py
+++ music_player/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import os
 from .models import *
-
 # Create your views here.
 def home(request):
-    music = Song.objects.all()
-    return render(request, 'home.html', {'songs':music})
+    songs = Song.objects.all()
+    return render(request, 'home.html', {'songs':songs})
+
+def song(request):
+    music = Song.objects.get(name='Monolit')
+    return render(request, 'song.html', {'song': music})
+
+
+
