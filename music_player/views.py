a460cb996dcc94f616dfed7fba2e6799ef078de9
DuhLessMax <130593272+DuhLessMax@users.noreply.github.com>
2023-09-06 20:15:27 +0300

    views

--- music_player/views.py
+++ music_player/views.py
@@ -3,8 +3,8 @@ import os
 from .models import *
 # Create your views here.
 def home(request):
-    music = Song.objects.all()
-    return render(request, 'home.html', {'songs':music})
+    songs = Song.objects.all()
+    return render(request, 'home.html', {'songs':songs})
 
 
 def song(request):
