51e6108bb7fd412094dbdc1fe1a62305c334975f
DuhLessMax <130593272+DuhLessMax@users.noreply.github.com>
2023-09-11 21:34:26 +0300

    33

--- music_player/forms.py
+++ music_player/forms.py
@@ -5,12 +5,14 @@ class SongForm (forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(
         attrs={'class': 'input'}
     ), label='Название трека')
-    song = forms.FileField(label='Файл трека')
-    song_image = forms.ImageField(label='Обложка трека', required=False)
+    
     singer = forms.CharField(widget=forms.TextInput(
         attrs={'class': 'input'}
     ), label='Исполнитель')
 
+    song = forms.FileField(label='Файл трека')
+    song_image = forms.ImageField(label='Обложка трека', required=False)
+    
     class Meta:
         model = Song
         fields = ['name', 'song', 'song_image', 'singer']
