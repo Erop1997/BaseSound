8ed314eb1bf3c0575e6d7c032b5d8173d890555c
DuhLessMax <130593272+DuhLessMax@users.noreply.github.com>
2023-09-20 20:30:07 +0300

    1

--- users/forms.py
+++ users/forms.py
@@ -13,20 +13,15 @@ class SignInForm(AuthenticationForm):
 
 class SignUpForm(UserCreationForm):
    username = forms.CharField(label='Придумайте никнейм')
-<<<<<<< HEAD
    password1 = forms.CharField(widget=forms.PasswordInput(), label='Придумайте пароль')          
    password2 = forms.CharField(widget=forms.PasswordInput(), label='Повторите пароль')
    email = forms.EmailField(label='Введите адресс эл.почты')
-=======
-   password1 = forms.CharField(widget=forms.PasswordInput, label='Придумайте пароль')          
-   password2 = forms.CharField(widget=forms.PasswordInput, label='Повторите пароль')
-   email = forms.EmailField('Введите адресс эл.почты')
->>>>>>> b86369d7d518cab58d85c462a8cf8ccdd3e30ec5
+
 
    class Meta:
        model = User
        fields = ['username', 'password1', 'password2', 'email']  
-<<<<<<< HEAD
+
 
 class ResetPasswordForm(PasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput(), label='Текущий пароль')
@@ -36,5 +31,4 @@ class ResetPasswordForm(PasswordChangeForm):
     class Meta:
         model = User
         fields = ['old_password', 'new_password1', 'new_password2'] 
-=======
->>>>>>> b86369d7d518cab58d85c462a8cf8ccdd3e30ec5
+
